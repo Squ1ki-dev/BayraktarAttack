ec0018be3caa25992c5897434a5715e31becd32b
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-04-03 23:25:59 +0300

    fix save skins && update tanks and drones materials

--- Assets/_Scripts/ProjectRunner.cs
+++ Assets/_Scripts/ProjectRunner.cs
@@ -9,16 +9,18 @@ public class ProjectRunner : MonoBehaviour
     public Transform skinPlace;
     public bool isFirstSession
     {
-        get { return !PlayerPrefsPro.Get<bool>(nameof(isFirstSession)); }
-        private set { PlayerPrefsPro.Set(nameof(isFirstSession), true); }
+        get { return PlayerPrefsPro.Get<bool>(nameof(isFirstSession)); }
+        private set { PlayerPrefsPro.Set(nameof(isFirstSession), value); }
     }
     void Start()
     {
         if (isFirstSession)
         {
+            isFirstSession = false;
             GameSaves.Instance.selectedDroneName.value = GameConfigs.Instance.shopSettings.drones[0].drone.name;
+            Debug.LogError(isFirstSession);
         }
-        
+
         SkinSelector skinSelector = new(skinPlace);
         WindowManager.Instance.Show<MainScreen>().Show(
             onPlay: () =>
--- Assets/_Scripts/View/SkinSelector.cs
+++ Assets/_Scripts/View/SkinSelector.cs
@@ -14,6 +14,8 @@ public class SkinSelector
         {
             drones.ForEach(d => d.Item1.SetActive(false));
             drones[idx].Item1.SetActive(true);
+            Debug.LogError(drones.FindIndex(item => item.Item2 == saves.selectedDroneName.value));
+            Debug.LogError(saves.selectedDroneName.value);
         });
     }
     ShopSettings shopSettings => GameConfigs.Instance.shopSettings;
