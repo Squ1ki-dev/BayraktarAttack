2f20161c944e6c8298a6845b3be3852cfbe21cb1
Squ1ki <70817453+Squ1ki-dev@users.noreply.github.com>
2023-11-30 21:40:56 +0000

    Update Dron.cs

--- Assets/_Scripts/Dron/Dron.cs
+++ Assets/_Scripts/Dron/Dron.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Partial definition of the 'Dron'(Base) class, inheriting from MonoBehaviour.
 public partial class Dron : MonoBehaviour
 {
+    // Reference to the CharacterController component attached to the same GameObject.
     private CharacterController chController;
+
+    // Serialized field for the view transform.
     [SerializeField] private Transform view;
+
+    // Awake method called when the script instance is being loaded.
     private void Awake()
     {
+        // Get the CharacterController component attached to the same GameObject.
         chController = GetComponent<CharacterController>();
     }
 }
