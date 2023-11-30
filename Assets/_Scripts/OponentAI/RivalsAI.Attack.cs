c9fc180172273edb0366edf0c61631418f1250f4
Squ1ki <70817453+Squ1ki-dev@users.noreply.github.com>
2023-11-30 21:39:50 +0000

    Update RivalsAI.Attack.cs

--- Assets/_Scripts/OponentAI/RivalsAI.Attack.cs
+++ Assets/_Scripts/OponentAI/RivalsAI.Attack.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public partial class RivalsAI
 {
+    // Method to handle attacking behavior.
     private void AttackTarget()
     {
+        // Check if the target for attack is dead; if so, reset the target.
         if(targetForAttack.IsDead) targetForAttack = null;
+
+        // If there is no valid target, return without further action.
         if (targetForAttack == null) return;
+
+        // Move the drone towards the position of the target tank.
         MoveDroneToTarget(targetForAttack.transform.position);
+
+        // Attempt to shoot at the target tank.
         dron.ShootIfHasTarget(tank =>
         {
+            // Reset the target after successfully shooting, and increment the player's score.
             targetForAttack = null;
             model.Scores.value++;
         });
